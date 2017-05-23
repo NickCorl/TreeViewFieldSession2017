78247db68d747ae3a725cfd99b3218959ab7913c
Ben Tarman <btarman@mymail.mines.edu>
2017-05-23 16:29:10 -0600

    deleted unneccessary files

--- src/aura/expenseList/expenseListController.js
+++
@@ -1,8 +0,0 @@
-({
-    update: function(component, evt, helper) {
-      var expense = component.get("v.expense");
-      // Note that updateExpense matches the name attribute in <aura:registerEvent>
-      var updateEvent = component.getEvent("updateExpense");
-      updateEvent.setParams({ "expense": expense }).fire();
-    }
-})
--- src/aura/node/nodeController.js
+++
@@ -1,6 +0,0 @@
-({
-	toggle : function(component, event, helper) {
-       //get expansion and set to the opposite value
-        component.set("v.expanded", !component.get("v.expanded"));
-	}
-})
